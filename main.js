91956b563b5686073c78eaad1ae1eb8ee991bd86
jun9130 <jun.nakama1030@gmail.com>
2023-03-21 13:20:52 +0900

    ファイルを読み込む

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // commanderモジュールからprogramオブジェクトをインポートする
 import { program } from "commander";
+// fs/promisesモジュール全体を読み込む
+import * as fs from "node:fs/promises";
 
-// コマンドライン引数をcommanderでパースする
+// コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
-
-// ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+  console.log(file);
+}).catch(err => {
+  console.error(err.message);
+  // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+  process.exit(1);
+});
